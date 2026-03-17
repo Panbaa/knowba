a78c0f8e2341ca7374d0dbdf1c6b0112d7c12592
Panba <gilbert.lynsche@gmx.de>
2026-03-17 18:16:28 +0100

    update ollama_client and main

--- backend/ollama_client.py
+++ backend/ollama_client.py
@@ -1,5 +1,18 @@
 from ollama import ChatResponse, chat
 
+chat_model = "llama3:8b"
+embedding_model = "qwen3-embedding"
+
+
 def prompt_ollama(messages):
     response: ChatResponse = chat(model="llama3:8b", messages=messages)
     return response
+
+def get_embedding(input_text: [str]):
+    response = ollama.embed(
+        model=embedding_model,
+        input=input_text
+    )
+    return response['embeddings']
+
+# get_embedding(["hello world", "how are you?"])
